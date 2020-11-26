f05f710853688b48ea07b2a30219324250130a61
PalumboN <nahuel.palumbo@gmail.com>
2020-11-26 12:01:04 -0300

    Renombro db -> baseDeDatos

--- src/Vista/App.tsx
+++ src/Vista/App.tsx
@@ -2,11 +2,18 @@ import logo from './img/logo.svg'
 import carpi from './img/carpi.png'
 import './App.css'
 import { NaveDeCarga, NaveDeResiduos, NaveDePasajeros, NaveDeCombate } from '../Dominio/naves'
-import db from '../Persistencia/db'
+import baseDeDatos from '../Persistencia/db'
 import Nave from './Nave'
 import React from 'react'
 
-export const naves = db.cargarNaves() || [new NaveDeCarga(), new NaveDeCarga(), new NaveDeResiduos(), new NaveDePasajeros(15), new NaveDePasajeros(20), new NaveDeCombate()]
+export const naves = baseDeDatos.cargarNaves() || [ 
+  new NaveDeCarga(), 
+  new NaveDeCarga(), 
+  new NaveDeResiduos(), 
+  new NaveDePasajeros(15), 
+  new NaveDePasajeros(20), 
+  new NaveDeCombate()
+]
 
 function App() {
   return (
@@ -14,12 +21,12 @@ function App() {
       <header className="App-header">
         <h1>Naves Espaciales</h1>
         <p><code>con ReactJS</code> <a target="blank" href="https://reactjs.org/"><img src={logo} className="App-logo" alt="logo" /></a></p>
-        <img src={carpi} className="Carpincho" alt="logo" />
+        <img src={carpi} className="Carpincho" alt="Carpi" />
       </header>
       {naves.map((unaNave, i) => <Nave key={i} nave={unaNave} />)}
       <div className="Persistencia-Acciones">
-        <button onClick={() => db.guardarNaves(naves)}>Guardar</button> {/*TODO: Recargar pagina */}
-        <button onClick={() => db.borrarTodo()}>Borrar</button>         {/*TODO: Recargar pagina */}
+        <button onClick={() => baseDeDatos.guardarNaves(naves)}>Guardar</button> {/*TODO: Recargar pagina */}
+        <button onClick={() => baseDeDatos.borrarTodo()}>Borrar</button>         {/*TODO: Recargar pagina */}
       </div>
     </div>
   )
